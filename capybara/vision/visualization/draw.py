c3ccf3e325cb90f2e627fdb177a06017343de179
Jack Lin <p12922005@ntu.edu.tw>
2024-12-19 20:23:51 +0800

    [A] Add prepare function for multi typing and add draw functions

--- capybara/vision/visualization/draw.py
+++ capybara/vision/visualization/draw.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 from typing import List, Tuple, Union
 
 import cv2
@@ -6,22 +6,28 @@ import matplotlib
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
-from ...structures import Box, Boxes, Polygon, Polygons
+from ...structures import Polygons
+from ...structures.boxes import _Box, _Boxes
+from ...structures.keypoints import _Keypoints, _KeypointsList
+from ...structures.polygons import _Polygon, _Polygons
 from ...utils import download_from_docsaid, get_curdir
 from ..geometric import imresize
+from .utils import (_Color, _Colors, _Point, _Points, _Scale, _Scales,
+                    _Thickness, _Thicknesses, prepare_box, prepare_boxes,
+                    prepare_color, prepare_colors, prepare_img,
+                    prepare_keypoints, prepare_keypoints_list, prepare_point,
+                    prepare_points, prepare_polygon, prepare_polygons,
+                    prepare_scale, prepare_scales, prepare_thickness,
+                    prepare_thicknesses)
 
 __all__ = [
     'draw_box', 'draw_boxes', 'draw_polygon', 'draw_polygons', 'draw_text',
     'generate_colors', 'draw_ocr_infos', 'draw_mask',
+    # 'draw_point', 'draw_points',
+    # 'draw_keypoints', 'draw_keypoints_list',
 ]
 
 DIR = get_curdir(__file__)
-
-
-_Color = Union[int, Tuple[int, int, int], np.ndarray]
-_Colors = Union[_Color, List[_Color], np.ndarray]
-_Thickness = Union[int, float, np.ndarray]
-_Thicknesses = Union[List[_Thickness], _Thickness, np.ndarray]
 
 if not (font_path := DIR / "NotoSansMonoCJKtc-VF.ttf").exists():
     file_id = "DiSqmnitNm3ZTPj"
@@ -30,7 +36,7 @@ if not (font_path := DIR / "NotoSansMonoCJKtc-VF.ttf").exists():
 
 def draw_box(
     img: np.ndarray,
-    box: Union[Box, np.ndarray],
+    box: _Box,
     color: _Color = (0, 255, 0),
     thickness: _Thickness = 2
 ) -> np.ndarray:
@@ -40,7 +46,7 @@ def draw_box(
     Args:
         img (np.ndarray):
             The image to draw on, as a numpy ndarray.
-        box (Union[Box, np.ndarray]):
+        box (_Box):
             The bounding box to draw, either as a Box object or as a numpy
             ndarray of the form [x1, y1, x2, y2].
         color (_Color, optional):
@@ -51,24 +57,22 @@ def draw_box(
     Returns:
         np.ndarray: The image with the drawn box, as a numpy ndarray.
     """
-    if img.ndim == 2:
-        img = np.stack([img] * 3, axis=-1)
-    if isinstance(box, Box):
-        if box.normalized:
-            h, w = img.shape[:2]
-            box = box.denormalize(w, h)
-        box = box.convert('XYXY').numpy()
-    elif not isinstance(box, np.ndarray):
-        box = np.array(box)
-    x1, y1, x2, y2 = box.astype(int)
+    img = prepare_img(img)
+    box = prepare_box(box)
+    color = prepare_color(color)
+    thickness = prepare_thickness(thickness)
+    if box.is_normalized:
+        h, w = img.shape[:2]
+        box = box.denormalize(w, h)
+    x1, y1, x2, y2 = box.numpy().astype(int).tolist()
     return cv2.rectangle(img, (x1, y1), (x2, y2), color=color, thickness=thickness)
 
 
 def draw_boxes(
     img: np.ndarray,
-    boxes: Union[Boxes, np.ndarray],
-    color: _Colors = (0, 255, 0),
-    thickness: _Thicknesses = 2
+    boxes: _Boxes,
+    colors: _Colors = (0, 255, 0),
+    thicknesses: _Thicknesses = 2
 ) -> np.ndarray:
     """
     Draws multiple bounding boxes on the image.
@@ -76,7 +80,7 @@ def draw_boxes(
     Args:
         img (np.ndarray):
             The image to draw on, as a numpy ndarray.
-        boxes (Union[Boxes, np.ndarray]):
+        boxes (_Boxes):
             The bounding boxes to draw, either as a list of Box objects or as a
             2D numpy ndarray.
         color (_Colors, optional):
@@ -89,23 +93,17 @@ def draw_boxes(
     Returns:
         np.ndarray: The image with the drawn boxes, as a numpy ndarray.
     """
-    # If color is not a list, make it a list
-    if not isinstance(color, list):
-        color = [color] * len(boxes)
-
-    # If thickness is not a list, make it a list
-    if not isinstance(thickness, list):
-        thickness = [thickness] * len(boxes)
-
-    for box, c, t in zip(boxes, color, thickness):
+    boxes = prepare_boxes(boxes)
+    colors = prepare_colors(colors, len(boxes))
+    thicknesses = prepare_thicknesses(thicknesses, len(boxes))
+    for box, c, t in zip(boxes, colors, thicknesses):
         draw_box(img, box, color=c, thickness=t)
-
     return img
 
 
 def draw_polygon(
     img: np.ndarray,
-    polygon: Union[Polygon, np.ndarray],
+    polygon: _Polygon,
     color: _Color = (0, 255, 0),
     thickness: _Thickness = 2,
     fillup=False,
@@ -117,7 +115,7 @@ def draw_polygon(
     Args:
         img (np.ndarray):
             The input image on which the polygon will be drawn.
-        polygon (Union[Polygon, np.ndarray]):
+        polygon (_Polygon):
             The points of the polygon. It can be either a list of points in the
             format [(x1, y1), (x2, y2), ...] or a Polygon object.
         color (Tuple[int, int, int], optional):
@@ -133,31 +131,28 @@ def draw_polygon(
     Returns:
         np.ndarray: The image with the drawn polygon.
     """
-    if img.ndim == 2:
-        img = np.stack([img] * 3, axis=-1)
-    if isinstance(polygon, Polygon):
-        if polygon.normalized:
-            h, w = img.shape[:2]
-            polygon = polygon.denormalize(w, h)
-        polygon = polygon.numpy()
-    elif not isinstance(polygon, np.ndarray):
-        polygon = np.array(polygon)
-    polygon = polygon.astype(int)
+    img = prepare_img(img)
+    polygon = prepare_polygon(polygon)
+    color = prepare_color(color)
+    thickness = prepare_thickness(thickness)
+    if polygon.is_normalized:
+        h, w = img.shape[:2]
+        polygon = polygon.denormalize(w, h)
+    polygon = polygon.numpy().astype(int).tolist()
 
     if fillup:
         img = cv2.fillPoly(img, [polygon], color=color, **kwargs)
     else:
-        img = cv2.polylines(img, [polygon], isClosed=True, color=color,
-                            thickness=thickness, **kwargs)
+        img = cv2.polylines(img, [polygon], isClosed=True, color=color, thickness=thickness, **kwargs)
 
     return img
 
 
 def draw_polygons(
     img: np.ndarray,
-    polygons: Polygons,
-    color: _Colors = (0, 255, 0),
-    thickness: _Thicknesses = 2,
+    polygons: _Polygons,
+    colors: _Colors = (0, 255, 0),
+    thicknesses: _Thicknesses = 2,
     fillup=False,
     **kwargs
 ):
@@ -167,17 +162,17 @@ def draw_polygons(
     Args:
         img (np.ndarray):
             The input image on which the polygons will be drawn.
-        polygons (List[Union[Polygon, np.ndarray]]):
+        polygons (_Polygons):
             A list of polygons to draw. Each polygon can be represented either
             as a list of points in the format [(x1, y1), (x2, y2), ...] or as a
             Polygon object.
-        color (_Colors, optional):
+        colors (_Colors, optional):
             The color(s) of the polygons in BGR format.
             If a single color is provided, it will be used for all polygons.
             If multiple colors are provided, each polygon will be drawn with the
             corresponding color.
             Defaults to (0, 255, 0) (green).
-        thickness (_Thicknesses, optional):
+        thicknesses (_Thicknesses, optional):
             The thickness(es) of the polygons' edges.
             If a single thickness value is provided, it will be used for all
             polygons. If multiple thickness values are provided, each polygon
@@ -192,29 +187,21 @@ def draw_polygons(
     Returns:
         np.ndarray: The image with the drawn polygons.
     """
-
-    # If color is not a list, make it a list
-    if not isinstance(color, list):
-        color = [color] * len(polygons)
-
-    # If thickness is not a list, make it a list
-    if not isinstance(thickness, list):
-        thickness = [thickness] * len(polygons)
-
-    for polygon, c, t in zip(polygons, color, thickness):
-        draw_polygon(img, polygon, color=c, thickness=t,
-                     fillup=fillup, **kwargs)
-
+    polygons = prepare_polygons(polygons)
+    colors = prepare_colors(colors, len(polygons))
+    thicknesses = prepare_thicknesses(thicknesses, len(polygons))
+    for polygon, c, t in zip(polygons, colors, thicknesses):
+        draw_polygon(img, polygon, color=c, thickness=t, fillup=fillup, **kwargs)
     return img
 
 
 def draw_text(
     img: np.ndarray,
     text: str,
-    location: np.ndarray,
-    color: tuple = (0, 0, 0),
+    location: _Point,
+    color: _Color = (0, 0, 0),
     text_size: int = 12,
-    font_path: str = None,
+    font_path: Union[str, Path] = None,
     **kwargs
 ) -> np.ndarray:
     """
@@ -225,9 +212,9 @@ def draw_text(
             Image on which to draw the text.
         text (str):
             Text string to be drawn.
-        location (np.ndarray):
+        location (_Point):
             x, y coordinates on the image where the text should be drawn.
-        color (tuple, optional):
+        color (_Color, optional):
             RGB values of the text color. Default is black (0, 0, 0).
         text_size (int, optional):
             Size of the text to be drawn. Default is 12.
@@ -241,8 +228,8 @@ def draw_text(
     Returns:
         np.ndarray: Image with the text drawn on it.
     """
-    if isinstance(img, np.ndarray):
-        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    img = prepare_img(img)
+    img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
     draw = ImageDraw.Draw(img)
     font_path = DIR / "NotoSansMonoCJKtc-VF.ttf" if font_path is None else font_path
@@ -253,11 +240,154 @@ def draw_text(
     text_height = bottom - top
 
     offset_y = int(0.5 * (font.size - text_height) - offset[1])
-    location = location + (0, offset_y)
+    loc = prepare_point(location)
+    loc = (loc[0], loc[1] + offset_y)
     kwargs.update({'fill': (color[2], color[1], color[0])})
-    draw.text(location, text, font=font, **kwargs)
+    draw.text(loc, text, font=font, **kwargs)
     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
+    return img
+
+
+def draw_point(
+    img: np.ndarray,
+    point: _Point,
+    scale: _Scale = 1.0,
+    color: _Color = (0, 255, 0),
+    thickness: _Thickness = -1,
+) -> np.ndarray:
+    '''
+    Draw a point on the image.
+
+    Args:
+        img (np.ndarray):
+            Image on which to draw the point.
+        point (_Point):
+            The point to draw.
+        scale (_Scale, optional):
+            The scale of the point. Defaults to 1.0.
+        color (_Color, optional):
+            The color of the point. Defaults to (0, 255, 0).
+        thickness (_Thickness, optional):
+            The thickness of the point. Defaults to -1.
+
+    Returns:
+        np.ndarray: Image with the drawn point.
+    '''
+    is_gray_img = img.ndim == 2
+    img = prepare_img(img)
+    point = prepare_point(point)
+    color = prepare_color(color)
+    h, w = img.shape[:2]
+    size = 1 + (np.sqrt(h * w) * 0.002 * scale).round().astype(int).item()
+    img = cv2.circle(img, point, radius=size, color=color, lineType=cv2.LINE_AA, thickness=thickness)
+    img = img[..., 0] if is_gray_img else img
+    return img
+
+
+def draw_points(
+    img: np.ndarray,
+    points: _Points,
+    scales: _Scales = 1.,
+    colors: _Colors = (0, 255, 0),
+    thicknesses: _Thicknesses = -1,
+) -> np.ndarray:
+    '''
+    Draw multiple points on the image.
+
+    Args:
+        img (np.ndarray):
+            Image on which to draw the points.
+        points (_Points):
+            The points to draw.
+        scales (_Scales, optional):
+            The scales of the points. Defaults to 1..
+        colors (_Colors, optional):
+            The colors of the points. Defaults to (0, 255, 0).
+        thicknesses (_Thicknesses, optional):
+            The thicknesses of the points. Defaults to -1.
+
+    Returns:
+        np.ndarray: Image with the drawn points.
+    '''
+    img = prepare_img(img).copy()
+    points = prepare_points(points)
+    colors = prepare_colors(colors, len(points))
+    thicknesses = prepare_thicknesses(thicknesses, len(points))
+    scales = prepare_scales(scales, len(points))
+
+    for p, s, c, t in zip(points, scales, colors, thicknesses):
+        img = draw_point(img, p, s, c, t)
+
+    return img
+
+
+def draw_keypoints(
+    img: np.ndarray,
+    keypoints: _Keypoints,
+    scale: _Scale = 1.,
+    thickness: _Thickness = -1
+) -> np.ndarray:
+    '''
+    Draw keypoints on the image.
+
+    Args:
+        img (np.ndarray):
+            Image on which to draw the keypoints.
+        keypoints (_Keypoints):
+            The keypoints to draw.
+        scale (float, optional):
+            The scale of the keypoints. Defaults to 1..
+        thickness (_Thickness, optional):
+            The thickness of the keypoints. Defaults to -1.
+
+    Returns:
+        np.ndarray: Image with the drawn keypoints.
+    '''
+    img = prepare_img(img)
+    keypoints = prepare_keypoints(keypoints)
+
+    if keypoints.is_normalized:
+        h, w = img.shape[:2]
+        keypoints = keypoints.denormalize(w, h)
+
+    colors = prepare_colors(np.array(keypoints.point_colors), len(keypoints))
+    scale = prepare_scale(scale)
+    thickness = prepare_thickness(thickness)
+    points = keypoints.numpy()[..., :2]
+    for p, c in zip(points, colors):
+        img = draw_point(img, p, scale, c, thickness)
+    return img
+
+
+def draw_keypoints_list(
+    img: np.ndarray,
+    keypoints_list: _KeypointsList,
+    scales: _Scales = 1.,
+    thicknesses: _Thicknesses = -1
+) -> np.ndarray:
+    '''
+    Draw keypoints list on the image.
+
+    Args:
+        img (np.ndarray):
+            Image on which to draw the keypoints.
+        keypoints_list (KeypointsList):
+            The keypoints list to draw.
+        scales (_Scales, optional):
+            The scales of the keypoints. Defaults to 1..
+        thicknesses (_Thicknesses, optional):
+            The thicknesses of the keypoints. Defaults to -1.
+
+    Returns:
+        np.ndarray: Image with the drawn keypoints list.
+    '''
+    img = prepare_img(img)
+    keypoints_list = prepare_keypoints_list(keypoints_list)
+    scales = prepare_scales(scales, len(keypoints_list))
+    thicknesses = prepare_thicknesses(thicknesses, len(keypoints_list))
+    for ps, s, t in zip(keypoints_list, scales, thicknesses):
+        img = draw_keypoints(img, ps, s, t)
     return img
 
 
