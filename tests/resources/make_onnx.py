1c89bcd3f57e37c31370bcd5d7d1432f57f8336d
kunkunlin <jacklin@cmlab.csie.ntu.edu.tw>
2025-01-15 17:52:45 +0800

    [A] add `make_onnx_dynamic_axes` and some test for onnxruntime

---
+++ tests/resources/make_onnx.py
@@ -0,0 +1,31 @@
+import capybara as cb
+
+cur_folder = cb.get_curdir(__file__)
+
+try:
+    import torch
+    model = torch.nn.Sequential(
+        torch.nn.Conv2d(3, 32, 3, 2, 1),
+        torch.nn.BatchNorm2d(32),
+        torch.nn.ReLU(),
+        torch.nn.Conv2d(32, 64, 3, 2, 1),
+        torch.nn.BatchNorm2d(64),
+        torch.nn.ReLU(),
+    )
+    torch.onnx.export(
+        model,
+        torch.randn(1, 3, 224, 224),
+        cur_folder/'model_shape=224x224.onnx',
+        input_names=['input'],
+        output_names=['output'],
+    )
+    torch.onnx.export(
+        model,
+        torch.randn(1, 3, 224, 224),
+        cur_folder/'model_dynamic-axes.onnx',
+        input_names=['input'],
+        output_names=['output'],
+        dynamic_axes={'input': {0: 'b', 2: 'h', 3: 'w'}, 'output': {0: 'b', 2: 'h', 3: 'w'}},
+    )
+except ImportError:
+    print("PyTorch is not installed.")
