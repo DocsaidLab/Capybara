581ddf11797d162f89f39d9e53b547eb14f13e83
zephyr-sh <shaynling@gmail.com>
2026-01-10 19:11:34 +0800

    refactor: make package imports explicit

--- capybara/structures/__init__.py
+++ capybara/structures/__init__.py
@@ -1,4 +1,35 @@
-from .boxes import *
-from .functionals import *
-from .keypoints import *
-from .polygons import *
+from __future__ import annotations
+
+from .boxes import Box, Boxes, BoxMode
+from .functionals import (
+    calc_angle,
+    is_inside_box,
+    jaccard_index,
+    pairwise_intersection,
+    pairwise_ioa,
+    pairwise_iou,
+    poly_angle,
+    polygon_iou,
+)
+from .keypoints import Keypoints, KeypointsList
+from .polygons import JOIN_STYLE, Polygon, Polygons, order_points_clockwise
+
+__all__ = [
+    "JOIN_STYLE",
+    "Box",
+    "BoxMode",
+    "Boxes",
+    "Keypoints",
+    "KeypointsList",
+    "Polygon",
+    "Polygons",
+    "calc_angle",
+    "is_inside_box",
+    "jaccard_index",
+    "order_points_clockwise",
+    "pairwise_intersection",
+    "pairwise_ioa",
+    "pairwise_iou",
+    "poly_angle",
+    "polygon_iou",
+]
--- capybara/vision/videotools/__init__.py
+++ capybara/vision/videotools/__init__.py
@@ -1,2 +1,6 @@
-from .video2frames import *
-from .video2frames_v2 import *
+from __future__ import annotations
+
+from .video2frames import video2frames
+from .video2frames_v2 import video2frames_v2
+
+__all__ = ["video2frames", "video2frames_v2"]
